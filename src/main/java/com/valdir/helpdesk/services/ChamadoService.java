9cd976195d33fe9043916207e5169998f0e7058a
DiegodaGuia <diegodguia@gmail.com>
2022-06-30 15:05:16 -0300

    findAll para chamados

--- src/main/java/com/valdir/helpdesk/services/ChamadoService.java
+++ src/main/java/com/valdir/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.valdir.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,15 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
-	
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID:" + id));
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
+
 	}
 }
