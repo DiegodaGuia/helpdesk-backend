e6b4e6df0d18eb2eddc6937b2d4d7a354abd59a2
DiegodaGuia <diegodguia@gmail.com>
2022-07-01 11:37:39 -0300

    Autenticação

--- src/main/java/com/valdir/helpdesk/services/DBService.java
+++ src/main/java/com/valdir/helpdesk/services/DBService.java
@@ -3,6 +3,7 @@ package com.valdir.helpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.valdir.helpdesk.domain.Chamado;
@@ -13,29 +14,28 @@ import com.valdir.helpdesk.domain.enums.Prioridade;
 import com.valdir.helpdesk.domain.enums.Status;
 import com.valdir.helpdesk.repositories.ChamadoRepository;
 import com.valdir.helpdesk.repositories.ClienteRepository;
-import com.valdir.helpdesk.repositories.TecnicoRepository;
 
 @Service
 public class DBService {
 	
 	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 
 	public void instaciaDB() {
 		
-		Tecnico tec1 = new Tecnico(null, "diego guia", "05422376366", "diegodguia@gmail.com", "123");
+		Tecnico tec1 = new Tecnico(null, "diego guia", "05422376366", "diegodguia@gmail.com", encoder.encode("123"));
 		tec1.addPerfis(Perfil.ADMIN);
 
-		Cliente cli1 = new Cliente(null, "Larissa", "05422358641", "lari@gmail.com", "1235");
+		Cliente cli1 = new Cliente(null, "Larissa", "05422358641", "lari@gmail.com", encoder.encode("123"));
 
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "chamado 01", "primeiro chamado", tec1,
 				cli1);
 
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
